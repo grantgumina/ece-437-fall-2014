8b9cc8686a75e10c5aa54cd2becff5c3659aeed6
Warren <warrengetlin@gmail.com>
2014-09-24 14:25:51 -0400

    calm before the shit storm

--- source/pipeline_ifid.sv
+++ source/pipeline_ifid.sv
@@ -3,11 +3,11 @@
 module pipeline_ifid
 import cpu_types_pkg::*;
 (
-	input logic CLK, nRST, en,
+	input logic CLK, nRST,
 	pipeline_if.ifid plif_ifid
 );
 
-always @(posedge clk or negedge rst_n) begin
+always @(posedge clk or negedge nRST) begin
 	if(~nRST) begin
 		plif_ifid.instr_l <= 0;
 	end else if(plif_ifid.en) begin
