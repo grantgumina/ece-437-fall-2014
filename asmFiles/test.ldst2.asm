7c640d7d5afaa765e3dee8f726fb7b368aa1b172
Warren <warrengetlin@gmail.com>
2014-09-26 13:34:28 -0400

    Still working, fixed hzif

--- asmFiles/test.ldst2.asm
+++ asmFiles/test.ldst2.asm
@@ -5,18 +5,39 @@
 
   org   0x0000
   lui   $10, 0xFEED
+  nop
+  nop
+  nop
   ori   $1, $zero, 0x0F00
   ori   $2, $zero, 0x0800
   ori   $10, $10, 0xBEEF
   nop
+  nop
+  nop
   lw    $3,0($1)
+  nop
+  nop
+  nop
   lw    $4,4($1)
+  nop
+  nop
+  nop
   lw    $5,8($1)
   nop
   nop
+  nop
   sw    $3,0($2)
+  nop
+  nop
+  nop
   sw    $4,4($2)
+  nop
+  nop
+  nop
   sw    $5,8($2)
+  nop
+  nop
+  nop
   sw    $10,12($2)
   halt      # that's all
 
--- source/hazard_unit.sv
+++ source/hazard_unit.sv
@@ -14,66 +14,74 @@ import cpu_types_pkg::*;
 	input CLK, nRST,
 	hazard_unit_if hzif
 );
+	assign hzif.rambusy = (hzif.dmemWEN || hzif.dmemREN) && !hzif.ihit;
 
-	always_ff @ (posedge CLK, negedge nRST) begin
-		if (~nRST) begin
-			hzif.ifid_sRST  <= 0;
-			hzif.ifid_en    <= 1;
-			hzif.idex_sRST  <= 0;
-			hzif.idex_en    <= 1;
-			hzif.exmem_sRST <= 0;
-			hzif.exmem_en   <= 1;
-			hzif.memwb_sRST <= 0;
-			hzif.memwb_en   <= 1;
+	always_comb begin: MEMORY
+		hzif.ifid_sRST  = 0;
+		hzif.ifid_en    = 1;
+		hzif.idex_sRST  = 0;
+		hzif.idex_en    = 1;
+		hzif.exmem_sRST = 0;
+		hzif.exmem_en   = 1;
+		hzif.memwb_sRST = 0;
+		hzif.memwb_en   = 1;
+		if (hzif.dmemWEN || hzif.dmemREN) begin
+			hzif.ifid_sRST  = 0;
+			hzif.ifid_en    = 0; //stalling
+			hzif.idex_sRST  = 0;
+			hzif.idex_en    = 0; //stalling
+			hzif.exmem_sRST = 0;
+			hzif.exmem_en   = 0; //stalling
+			hzif.memwb_sRST = 0; 
+			hzif.memwb_en   = 1;
+			if (hzif.dhit) begin
+				hzif.ifid_sRST  = 0;
+				hzif.ifid_en    = 0; //stalling
+				hzif.idex_sRST  = 0;
+				hzif.idex_en    = 0; //stalling
+				hzif.exmem_sRST = 1; //nopping
+				hzif.exmem_en   = 0; 
+				hzif.memwb_sRST = 0;
+				hzif.memwb_en   = 1; 
+			end
+		end
+		else if (hzif.ihit) begin
+			hzif.ifid_sRST  = 0; //everything resumes
+			hzif.ifid_en    = 1; 
+			hzif.idex_sRST  = 0;
+			hzif.idex_en    = 1; 
+			hzif.exmem_sRST = 0; 
+			hzif.exmem_en   = 1; 	
+			hzif.memwb_sRST = 0;
+			hzif.memwb_en   = 1;
 		end
 		else begin
-			if (hzif.dmemWEN || hzif.dmemREN) begin
-				hzif.ifid_sRST  <= 0;
-				hzif.ifid_en    <= 0; //stalling
-				hzif.idex_sRST  <= 0;
-				hzif.idex_en    <= 0; //stalling
-				hzif.exmem_sRST <= 0;
-				hzif.exmem_en   <= 0; //stalling
-				hzif.memwb_sRST <= 0; 
-				hzif.memwb_en   <= 1;
-			end
-			else if (hzif.dhit) begin
-				hzif.ifid_sRST  <= 0;
-				hzif.ifid_en    <= 0; //stalling
-				hzif.idex_sRST  <= 0;
-				hzif.idex_en    <= 1; //resume
-				hzif.exmem_sRST <= 1; //nopping
-				hzif.exmem_en   <= 0; 
-				hzif.memwb_sRST <= 0;
-				hzif.memwb_en   <= 1;
-			end
-			else if (hzif.ihit) begin
-				hzif.ifid_sRST  <= 0;
-				hzif.ifid_en    <= 1;
-				hzif.idex_sRST  <= 0;
-				hzif.idex_en    <= 1; //resuming
-				hzif.exmem_sRST <= 0; //resume
-				hzif.exmem_en   <= 1;	//resumed	
-				hzif.memwb_sRST <= 0;
-				hzif.memwb_en   <= 1;
-			end
-			else begin
-				hzif.ifid_sRST  <= 0;
-				hzif.ifid_en    <= 1;
-				hzif.idex_sRST  <= 0;
-				hzif.idex_en    <= 1;
-				hzif.exmem_sRST <= 0;
-				hzif.exmem_en   <= 1;
-				hzif.memwb_sRST <= 0;
-				hzif.memwb_en   <= 1;
-			end
+			hzif.ifid_sRST  = 0;
+			hzif.ifid_en    = 1;
+			hzif.idex_sRST  = 0;
+			hzif.idex_en    = 1;
+			hzif.exmem_sRST = 0;
+			hzif.exmem_en   = 1;
+			hzif.memwb_sRST = 0;
+			hzif.memwb_en   = 1;
 		end
 	end
-
-	assign hzif.rambusy = (hzif.dmemWEN || hzif.dmemREN) && !hzif.ihit;
-	
-
 endmodule
+
+/*
+always_ff @ (posedge CLK, negedge nRST) begin: DATA
+	if (~nRST) begin
+			hzif.ifid_sRST  = 0;
+			hzif.ifid_en    = 1;
+			hzif.idex_sRST  = 0;
+			hzif.idex_en    = 1;
+			hzif.exmem_sRST = 0;
+			hzif.exmem_en   = 1;
+			hzif.memwb_sRST = 0;
+			hzif.memwb_en   = 1;		
+	end
+end
+*/
 
 
 
@@ -84,43 +92,43 @@ endmodule
 /*
 	always_ff @ (posedge CLK or negedge nRST) begin
 		if(~nRST) begin
-			hzif.imemREN_out <= 1;
-			hzif.dmemREN_out <= 0;
-			hzif.dmemWEN_out <= 0;
+			hzif.imemREN_out = 1;
+			hzif.dmemREN_out = 0;
+			hzif.dmemWEN_out = 0;
 		end else begin
 			if (hzif.ihit == 1) begin
-				hzif.dmemREN_out <= hzif.dmemREN_in;
-				hzif.dmemWEN_out <= hzif.dmemWEN_in;
-				// hzif.pcsrc_out <= hzif.pcsrc_in;
+				hzif.dmemREN_out = hzif.dmemREN_in;
+				hzif.dmemWEN_out = hzif.dmemWEN_in;
+				// hzif.pcsrc_out = hzif.pcsrc_in;
 			end else if (hzif.ihit == 0) begin
-				hzif.imemREN_out <= 1;
-				hzif.dmemREN_out <= 0;
-				hzif.dmemWEN_out <= 0;
+				hzif.imemREN_out = 1;
+				hzif.dmemREN_out = 0;
+				hzif.dmemWEN_out = 0;
 				if (hzif.dmemREN_in == 1 && hzif.dmemWEN_in == 0) begin
-					hzif.imemREN_out <= 1;
-					hzif.dmemREN_out <= 1;
-					hzif.dmemWEN_out <= 0;
+					hzif.imemREN_out = 1;
+					hzif.dmemREN_out = 1;
+					hzif.dmemWEN_out = 0;
 				end else if (hzif.dmemREN_in == 0 && hzif.dmemWEN_in == 1) begin
-					hzif.imemREN_out <= 1;
-					hzif.dmemREN_out <= 0;
-					hzif.dmemWEN_out <= 1;
+					hzif.imemREN_out = 1;
+					hzif.dmemREN_out = 0;
+					hzif.dmemWEN_out = 1;
 				end
 			end else if (hzif.dhit == 1) begin // same case as nRST
-				hzif.imemREN_out <= 1;
-				hzif.dmemREN_out <= 0;
-				hzif.dmemWEN_out <= 0;
+				hzif.imemREN_out = 1;
+				hzif.dmemREN_out = 0;
+				hzif.dmemWEN_out = 0;
 			end
 		end
 	end
 
 	always_ff @ (posedge CLK or negedge nRST) begin
 		if (~nRST) begin
-			hzif.regen_out <= 0;
+			hzif.regen_out = 0;
 		end else begin
 			if (hzif.ihit == 1) begin
-				hzif.regen_out <= hzif.regen_in;
+				hzif.regen_out = hzif.regen_in;
 			end else begin
-				hzif.regen_out <= 0;
+				hzif.regen_out = 0;
 			end
 		end
 	end
